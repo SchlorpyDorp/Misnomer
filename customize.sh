98b27a37f7fffa602e2456f01ee0e3025a90a101
SchlorpyDorp <exilir8des@gmail.com>
2026-07-19 04:05:46 +0200

    Installer appended

--- customize.sh
+++ customize.sh
@@ -1,18 +1,25 @@
 #!/system/bin/sh
 
 ui_print " "
-ui_print "================================="
-ui_print "          Misnomer v1.0.2"
-ui_print "================================="
+ui_print "====================================="
+ui_print "         Misnomer v1.0.3"
+ui_print "====================================="
 ui_print " "
-ui_print "- Installing Misnomer"
-ui_print "- Preparing configuration"
+ui_print "- Initialising installer..."
+ui_print "- Checking environment..."
+ui_print "- Preparing configuration..."
+ui_print "- Creating configuration directory..."
+ui_print "- Setting file permissions..."
+ui_print "- Installing WebUI assets..."
+ui_print "- Registering boot service..."
+ui_print "- Calibrating gaslighting engine..."
+ui_print "- Teaching Android to lie..."
 ui_print " "
 
 mkdir -p "$MODPATH/config"
 
 if [ ! -f "$MODPATH/config/props.conf" ]; then
-    cat > "$MODPATH/config/props.conf" << 'EOF'
+cat > "$MODPATH/config/props.conf" << 'EOF'
 MODEL=
 BRAND=
 MANUFACTURER=
@@ -26,5 +33,8 @@ set_perm "$MODPATH/post-fs-data.sh" 0 0 0755
 set_perm_recursive "$MODPATH/webroot" 0 0 0755 0644
 set_perm_recursive "$MODPATH/config" 0 0 0755 0644
 
-ui_print "- Installation complete"
+ui_print "- Installation complete."
+ui_print "- Reboot required."
+ui_print " "
+ui_print "Gaslight Android."
 ui_print " "
